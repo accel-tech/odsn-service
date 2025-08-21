416002c9d81c248763b6d6459e83d675945290b5
layedevX <papeousmane.ndiaye@accel-tech.net>
2025-08-21 09:23:49 +0000

    initialize project

--- src/main/java/heritage/africa/odsn_service/entity/Plainte.java
+++ src/main/java/heritage/africa/odsn_service/entity/Plainte.java
@@ -1,8 +1,5 @@
 package heritage.africa.odsn_service.entity;
 
-
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
